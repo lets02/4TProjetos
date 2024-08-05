9e7e7f38b370d04e4a4719fb3e344df4a4eacd02
lets02 <lelezanette00@icloud.com>
2024-08-05 13:37:07 -0300

    05/08/2024

---
+++ PHPProjects/meu-primeiro-laravel/resources/views/parts/nav.blade.php
@@ -0,0 +1,8 @@
+<div class="nav">
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/produtos">Produtos</a></li>
+        <li><a href="/contatos">Contatos</a></li>
+    </ul>
+
+</div>
