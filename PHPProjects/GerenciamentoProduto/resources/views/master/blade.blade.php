db69456438c9a14a3481b603a0f94fe76ce8f178
lets02 <lelezanette00@icloud.com>
2024-08-09 13:29:40 -0300

    09/08/2024

--- PHPProjects/GerenciamentoProduto/resources/views/master/blade.blade.php
+++ PHPProjects/GerenciamentoProduto/resources/views/master/blade.blade.php
@@ -1,3 +1,7 @@
-<div>
-    <!-- Nothing worth having comes easy. - Theodore Roosevelt -->
-</div>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Gerenciamento de Produtos</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+</html>
